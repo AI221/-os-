561bb9e3d01b431422410e486df5926f044dd296
jackson <aguyshat@gmail.com>
2015-08-31 20:53:36 -0500

    Update to 0.7

--- library/api/nokernel.lua
+++ library/api/nokernel.lua
@@ -6,10 +6,29 @@ Jackson McNeill
 Version: 
 1; for OS 0.6
 ]]
+
+
+function osStuff.menuBar()
+	
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function osStuff.cRun(file,args)
     assert(type(file)=="string","osStuff.cRun - expected string,table got ~~need a function to tell this")
     if not type(args)=="table" then args = {} end
-    if not env.fs.exists(file) then 
+    if not fs.exists(file) then 
         game.e("cRun - File("..file..") doesn't exist!")
         return 1
     elseif fs.isReadOnly(file)=="noAccess"then 
@@ -67,7 +86,7 @@ function osStuff.rightClick(tabl,x,y,backColor,textColor)
     --rBox:draw()
     return rBox
 end 
-local function bwrite( sText,w,newLnStart,width)--ripped from bios.lua
+local function bwrite( sText,w,newLnStart,width,extra)--ripped from bios.lua
     local _,h = term.getSize()        
     local x,y = term.getCursorPos()
     local str =""
@@ -134,21 +153,19 @@ local function bwrite( sText,w,newLnStart,width)--ripped from bios.lua
         end
     end
     local x,y=term.getCursorPos()
-    term.setCursorPos(newLnStart,y+1)
-    term.write(str)
-    term.setCursorPos(newLnStart,y+2)
-    term.write(str)
-    return nLinesPrinted
+    for i=1,extra or 2 do 
+        term.setCursorPos(newLnStart,y+i)
+        term.write(str)
+        term.setCursorPos(newLnStart,y+i)
+    end 
+
+    return y
 end
-
-
-
-function osStuff.popup(x,y,width,buttons,text,topText,dontuseXButton)
+local function popup(x,y,width,text,topText,extra)
     if type(x)~="number" then --might do with y later
         local tx,ty=term.getSize()
         x=math.floor((tx-width)/2)
     end 
-    --make the top of the window
     local buttonX = width+x
     if not osStuff.settings.windows.buttonsOnCorrectSide then 
         buttonX = x 
@@ -159,7 +176,13 @@ function osStuff.popup(x,y,width,buttons,text,topText,dontuseXButton)
     term.write(topText)
     term.setBackgroundColor(osStuff.settings.windows.windowsMiddleColor)
     term.setCursorPos(x,y+1)
-    bwrite(text,width+x,x,width)
+    return x,buttonX,bwrite(text,width+x,x,width,extra)
+end
+
+function osStuff.popup(x,y,width,buttons,text,topText,dontuseXButton)
+      --make the top of the window
+    local buttonX
+    x,buttonX=popup(x,y,width,text,topText)
     local _,by = term.getCursorPos()
     local butts={}
     if type(buttons)=="table" then 
@@ -177,15 +200,148 @@ function osStuff.popup(x,y,width,buttons,text,topText,dontuseXButton)
     game.drawButtons(butts)
     osStuff.renderMe() -- for apps using the self-rendering method
     while true do 
-        local evts={coroutine.yield("mouse_click")}
-        local txt,num=game.checkButtons(butts,evts)
-        if num == #butts and (not dontuseXButton) then 
-            return 
-        elseif num then 
-            return num 
+        local evts={coroutine.yield()}
+        if evts[1]=="mouse_click" then 
+            local txt,num=game.checkButtons(butts,evts)
+            if num == #butts and (not dontuseXButton) then 
+                return 
+            elseif num then 
+                return num 
+            end 
+        elseif evts[1]=="key" then 
+            if evts[2]==28 then 
+                return
+            elseif evts[2]==1 and (not dontuseXButton) then 
+                return 
+            end 
         end 
     end 
 end 
+function osStuff.textPopup(x,y,width,buttons,text,topText,dontuseXButton,astrickChar)
+      --make the top of the window
+    local buttonX
+    x,buttonX=popup(x,y,width,text,topText,4)
+    local _,by = term.getCursorPos()
+    local butts={}
+    if type(buttons)=="table" then 
+        term.setBackgroundColor(osStuff.settings.windows.textboxBgColor)
+        for i=1,#buttons do 
+            --term.setCursorPos(1,1)
+            --game.addButton(buttons[i].txt,buttons[i].x+x-1,by,osStuff.settings.windows.buttonColor,osStuff.settings.windows.windowsTextColor,butts)
+            buttons[i].txt,buttons[i].length,buttons[i].y=buttons[i].txt or "", buttons[i].length or (width-buttons[i].x)-2,buttons[i].y or 0
+            buttons[i].y = buttons[i].y+by-2
+            local txt = buttons[i].txt
+            for o=#buttons[i].txt,buttons[i].length do 
+                txt=txt.." "
+            end 
+            term.setCursorPos(buttons[i].x+x,buttons[i].y)
+            term.write(txt)
+        end 
+
+    end
+    game.addButton("Okay",  ((width-11)/2)+x,  by,osStuff.settings.windows.buttonColor,osStuff.settings.windows.windowsTextColor,butts)--this should be 12 but for some reason 11 works better. idk why.
+    game.addButton("Cancel",((width-11)/2)+x+6,by,osStuff.settings.windows.buttonColor,osStuff.settings.windows.windowsTextColor,butts)
+    if not dontuseXButton then 
+        game.addButton(osStuff.settings.windows.tbuttonX,buttonX,y,osStuff.settings.windows.cbuttonX,osStuff.settings.windows.windowsTextColor,butts)
+    end
+    game.drawButtons(butts)
+    osStuff.renderMe() -- for apps using the self-rendering method        
+
+
+    term.setCursorPos(buttons[1].x+x,buttons[1].y)
+    term.setBackgroundColor(osStuff.settings.windows.textboxBgColor)
+    buttons[1].txt = osStuff.readRecolored(astrickChar, nil, nil,osStuff.settings.windows.windowsTextColor,osStuff.settings.windows.windowsMiddleColor,buttons[1].txt,buttons[1].length+x+buttons[1].x)--w is the limit for the size
+
+    while true do 
+
+        local evts={coroutine.yield()}
+        if evts[1]=="mouse_click" then
+            local txt,num=game.checkButtons(butts,evts)
+            if ( num == 3 and (not dontuseXButton) ) or num==2 then 
+                return 
+            elseif num == 1 then 
+                local ta ={}
+                for i=1,#buttons do 
+                    ta[i]=buttons[i].txt
+                end 
+                return ta 
+            else
+                for i=1,#buttons do 
+                    if evts[3]>=buttons[i].x+x and evts[3] <=buttons[i].x+x+buttons[i].length and evts[4]==buttons[i].y then 
+                        term.setCursorPos(buttons[i].x+x,buttons[i].y)
+                        term.setBackgroundColor(osStuff.settings.windows.textboxBgColor)
+                        buttons[i].txt=osStuff.readRecolored(astrickChar, nil, nil,osStuff.settings.windows.windowsTextColor,osStuff.settings.windows.windowsMiddleColor,buttons[i].txt,buttons[i].length+x+buttons[i].x)--w is the limit for the size
+                    end 
+                end 
+            end 
+        elseif evts[1]=="key" then 
+            if evts[2]==28 then 
+                local ta ={}
+                for i=1,#buttons do 
+                    ta[i]=buttons[i].txt
+                end 
+                return ta 
+            elseif evts[2]==1 then 
+                return 
+            end 
+        end 
+    end 
+end 
+
+function osStuff.configPopup(x,y,width,buttons,text,topText,dontuseXButton)
+      --make the top of the window
+    local buttonX,bety
+    x,buttonX,bety=popup(x,y,width,text,topText,buttons.ly+buttons.x+(x-1))
+    local _,by = term.getCursorPos()
+    local butts=game.makeConfigBox(buttons,x+buttons.x-1,bety+1,buttons.lx,buttons.ly,osStuff.settings.windows.configBack,osStuff.settings.windows.configText)
+    
+    game.addButton("Okay",  ((width-11)/2)+x,  by,osStuff.settings.windows.buttonColor,osStuff.settings.windows.windowsTextColor,butts)--this should be 12 but for some reason 11 works better. idk why.
+    game.addButton("Cancel",((width-11)/2)+x+6,by,osStuff.settings.windows.buttonColor,osStuff.settings.windows.windowsTextColor,butts)
+    if not dontuseXButton then 
+        game.addButton(osStuff.settings.windows.tbuttonX,buttonX,y,osStuff.settings.windows.cbuttonX,osStuff.settings.windows.windowsTextColor,butts)
+    end
+    game.drawButtons(butts)
+    butts:draw()
+    osStuff.renderMe() -- for apps using the self-rendering method        
+    local shiftPressed=false 
+    while true do 
+
+        local evts={coroutine.yield()}
+        if evts[1]=="mouse_click" then
+            local txt,num=game.checkButtons(butts,evts)
+            if ( num == 3 and (not dontuseXButton) ) or num==2 then 
+                return 
+            elseif num == 1 then 
+                return 
+            else
+		local res = butts:check(evts)
+		if res then 
+			return res 
+		end
+            end 
+	elseif evts[1] == "key" and evts[2]==42 then --shift
+		shiftPressed=true
+	elseif evts[1]=="key_up" and evts[2]==42 then 
+		shiftPressed=false 
+	elseif evts[1]=="mouse_scroll" then
+--		evts[1]="mouse_click"
+		if butts:check(evts) then 
+			if not osStuff.settings.scrollCorrectly then
+				evts[2]=evts[2]*-1 
+			end 
+			if shiftPressed then 
+				butts:addScroll(evts[2])
+			else 
+				butts:addScroll(0,evts[2])
+			end
+			butts:draw()
+		end
+        elseif evts[1]=="key" and evts[2]==1 then 
+        	return 
+        end 
+    end 
+end 
+
 -- Add [os] help to the help app 
 local nativeLookup = help.lookup 
 local nativeTopics = help.topics
